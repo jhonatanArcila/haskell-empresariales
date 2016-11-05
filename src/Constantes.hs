c57215bcee4f2d6202c5b4a126b011f0ddad5214
ealejandrootalvaro <ealejandro.otalvaro@gmail.com>
2016-11-05 20:41:27 +0000

    Update Constantes.hs

--- src/Constantes.hs
+++ src/Constantes.hs
@@ -11,7 +11,7 @@ error' :: String
 error' = "Error"
 
 connectionStr :: ByteString
---connectionStr = "postgres://xlyrtaxdwdozqh:ml6B7YXEWvGLdpcw5ty-BDrcne@ec2-50-19-240-113.compute-1.amazonaws.com:5432/dcku066iig1lq1"
-connectionStr = "postgresql://postgres:94cbd72b4e4133f3417a61adf9a418b1@138.197.15.163:5454/restaurant"
+connectionStr = "postgres://xlyrtaxdwdozqh:ml6B7YXEWvGLdpcw5ty-BDrcne@ec2-50-19-240-113.compute-1.amazonaws.com:5432/dcku066iig1lq1"
+--connectionStr = "postgresql://postgres:94cbd72b4e4133f3417a61adf9a418b1@138.197.15.163:5454/restaurant"
 puerto :: Int
 puerto = 8087
